9de357a94ca37ac7d14f90690b451b2c183d7caa
VicenteLozano <vlozano.docencia@gmail.com>
2021-04-08 21:15:57 +0200

    Añadido alumno fRodriguez

--- src/main/java/es/edu/emiliojimeno/dam31/ed/alumnosed/Main.java
+++ src/main/java/es/edu/emiliojimeno/dam31/ed/alumnosed/Main.java
@@ -14,17 +14,16 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
-  
         Alumno vLozano = new Alumno("vLozano","20/21");
         alumnos.add(vLozano);
         Alumno rOrtega = new Alumno("rOrtega","20/21");
         alumnos.add(rOrtega);
-        
+        Alumno fRodriguez = new Alumno("fRodriguez", "20/21");
+        alumnos.add(fRodriguez);
         
         for(Alumno a : alumnos){
             System.out.println(a);
         }
-        
     }
     
 }
